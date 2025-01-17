158c94dafecb133a7b576812f6e47d466305ef9c
Jaewook Lee <wookboy00@naver.com>
2025-01-17 20:13:43 +0900

    프로젝트 정리 및 설정

--- build/Release/addon.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'dguard' {
-    export function encrypt(tableName: string, columnName: string, value: string): string;
-    export function decrypt(tableName: string, columnName: string, value: string): string;
-    export function hash(tableName: string, columnName: string, value: string): string;
-}
