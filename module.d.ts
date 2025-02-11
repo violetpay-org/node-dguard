46bb00d8727a318783232c9219a93c553d778d45
Jaewook Lee <wookboy00@naver.com>
2025-02-11 19:29:33 +0900

    Fix type declaration

--- module.d.ts
+++ module.d.ts
@@ -7,9 +7,42 @@ declare module 'dguard' {
         local?: boolean;
     }
 
+    /**
+     * DGuard를 초기화합니다.
+     * @param options
+     */
     export function init(options?: { local: boolean }): void;
+
+    /**
+     * value를 암호화합니다.
+     * @param tableName
+     * @param columnName
+     * @param value
+     * @throws {Error} KMS 오류로 암호화에 실패했을 때
+     */
     export function encrypt(tableName: string, columnName: string, value: string): Promise<string>;
+
+    /**
+     * value를 복호화합니다.
+     * @param tableName
+     * @param columnName
+     * @param value
+     * @throws {Error} value 값이 잘못됨
+     * @throws {Error} KMS 오류로 복호화에 실패
+     */
     export function decrypt(tableName: string, columnName: string, value: string): Promise<string>;
-    export function hash(tableName: string, columnName: string): Promise<void>;
+
+    /**
+     * value로 hash를 생성합니다.
+     * @param tableName
+     * @param columnName
+     * @param value
+     * @throws {Error} KMS 오류로 hash 생성에 실패
+     */
+    export function hash(tableName: string, columnName: string, value: string): Promise<string>;
+
+    /**
+     * DGuard를 종료합니다.
+     */
     export function close(): void;
 }
